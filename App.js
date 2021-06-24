da274f4749ef7684d6e94de99840ed0600aab1b2
kornatskyi <bohdan.kornatskyi@gmail.com>
2021-06-24 10:22:45 -0700

    Icons, Favorites, and Comments

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, YellowBox } from 'react-native';
+import { StyleSheet, Text, View, YellowBox,LogBox } from 'react-native';
 import Main from './components/MainComponent';
-YellowBox.ignoreWarnings([""]);
+LogBox.ignoreLogs(['']);
+
+// YellowBox.ignoreWarnings([""]);
 
 export default function App() {
   return (
