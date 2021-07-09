96a7ca971576e6ea0ac09b8730fd7f7e26fd8056
kornatskyi <bohdan.kornatskyi@gmail.com>
2021-07-09 15:04:40 -0700

    Secure Store

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -25,6 +25,8 @@ import {
 } from '../redux/ActionCreators';
 import Reservation from './ReservationComponent';
 import Favorites from './FavoritesComponent';
+import Login from './LoginComponent';
+
 
 
 const mapDispatchToProps = {
@@ -185,6 +187,29 @@ const ContactNavigator = createStackNavigator(
   }
 );
 
+const LoginNavigator = createStackNavigator(
+  {
+      Login: { screen: Login }
+  },
+  {
+      defaultNavigationOptions: ({navigation}) => ({
+          headerStyle: {
+              backgroundColor: '#5637DD'
+          },
+          headerTintColor: '#fff',
+          headerTitleStyle: {
+              color: '#fff'
+          },
+          headerLeft: <Icon
+              name='sign-in'
+              type='font-awesome'
+              iconStyle={styles.stackIcon}
+              onPress={() => navigation.toggleDrawer()}
+          />
+      })
+  }
+);
+
 const CustomDrawerContentComponent = (props) => (
   <ScrollView>
     <SafeAreaView
@@ -209,6 +234,19 @@ const CustomDrawerContentComponent = (props) => (
 
 const MainNavigator = createDrawerNavigator(
   {
+    Login: {
+      screen: LoginNavigator,
+      navigationOptions: {
+          drawerIcon: ({tintColor}) => (
+              <Icon
+                  name='sign-in'
+                  type='font-awesome'
+                  size={24}
+                  color={tintColor}
+              />
+          )
+      }
+  },
     Home: {
       screen: HomeNavigator,
       navigationOptions: {
@@ -217,6 +255,7 @@ const MainNavigator = createDrawerNavigator(
         ),
       },
     },
+    
     Directory: {
       screen: DirectoryNavigator,
       navigationOptions: {
@@ -282,6 +321,7 @@ const MainNavigator = createDrawerNavigator(
   },
 
   {
+    initialRouteName: 'Home',
     drawerBackgroundColor: "#CEC8FF",
     contentComponent: CustomDrawerContentComponent,
   }
