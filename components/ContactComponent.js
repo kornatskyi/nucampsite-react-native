df3daa15b61563908e103207c951624e10c031da
kornatskyi <bohdan.kornatskyi@gmail.com>
2021-07-09 15:19:09 -0700

    Local Notifications

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
 import { ScrollView, Text } from "react-native";
-import { Card } from "react-native-elements";
+import { Card, Button, Icon } from 'react-native-elements';
 import * as Animatable from 'react-native-animatable';
+import * as MailComposer from 'expo-mail-composer';
 
 export default class Contact extends Component {
   static navigationOptions = {
     title: "Contact Us",
   };
+
+
+
+
   render() {
     return (
       <ScrollView>
@@ -16,6 +21,17 @@ export default class Contact extends Component {
               {"1 Nucamp Way \nSeattle,WA 98001 \nU.S.A."}
             </Text>
             <Text>{"Phone: 1-206-555-1234 Email: \ncampsites@nucamp.co"}</Text>
+            <Button
+              title="Send Email"
+              buttonStyle={{ backgroundColor: '#5637DD', margin: 40 }}
+              icon={<Icon
+                name='envelope-o'
+                type='font-awesome'
+                color='#fff'
+                iconStyle={{ marginRight: 10 }}
+              />}
+              onPress={() => this.sendMail()}
+            />
           </Card>
         </Animatable.View>
       </ScrollView>
